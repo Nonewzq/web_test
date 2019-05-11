ef7542410a4d7925a0ebe6084628f02bdbe51d29
wangzhiqiang <409968627@qq.com>
2019-05-11 23:43:44 +0800

    day2.2

--- t1.py
+++ t1.py
@@ -1,17 +1,51 @@
 import socket
 
 
-def f1():
-    return b"f1,xxx"
+def f1(request):
+    """
+
+    :param request:
+    :return:
+    """
+    f = open("index.html", "rb")
+
+    data = f.read()
+    f.close()
+    return data
 
 
-def f2():
+def f2(request):
+    """
+
+    :param request:
+    :return:
+    """
     return b"f2,ooo"
 
+
+def f3(request):
+    import time
+    f = open("aricle.html", "r")
+    data = f.read()
+    data = data.replace("@@@test@@@", str(time.time()))
+    data = bytes(data, encoding="utf-8")
+    f.close()
+    return data
+
+
+def f4(request):
+
+    f = open("dbtest.html", "r")
+    data = f.read()
+    data = bytes(data,encoding="utf-8")
+    f.close()
+    return data
 
 routers = {
     "/xxx": f1,
     "/ooo": f2,
+    "/aricles": f3,
+    "/mysql":f4
 }
 
 
@@ -29,24 +63,18 @@ def run():
         temp_list = headers.split("\r\n")
         method, url, protocal = temp_list[0].split(" ")
         func_name = ""
+
         for item in routers:
             if item == url:
                 func_name = routers[item]
 
         if func_name:
-            response = func_name()
+            response = func_name(data)
         else:
             response = b"404"
-
+        conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
         conn.send(response)
-
-        # if url == "/xxx":
-        #     conn.send(b"123456")
-        # else:
-        #     conn.send(b"404 not found")
-
         print(data)
-
         conn.close()
 
 
